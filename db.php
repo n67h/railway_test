e347d9053d0346ad7c0e93ac6795433946cb5bf5
n67h <night67hunter@gmail.com>
2023-11-12 14:43:09 +0800

    Change database credentials

--- db.php
+++ db.php
@@ -1,10 +1,12 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "railway_db";
+    $DB_HOST = $_ENV["DB_HOST"];
+    $DB_USER = $_ENV["DB_USER"];
+    $DB_PASSWORD = $_ENV["DB_PASSWORD"];
+    $DB_NAME = $_ENV["DB_NAME"];
+    $DB_PORT = $_ENV["DB_PORT"];
 
-    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME, $DB_PORT);
 
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
